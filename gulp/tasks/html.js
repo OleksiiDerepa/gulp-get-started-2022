7e795d49ea3e6f5e243253e76d123ff112906225
qiceto <qiceto.atedod@rungel.net>
2024-06-27 17:17:30 +0300

    avoid browser cacheing for styles / images / files - install gulp-replace plugin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,6 @@
 import fileInclude from "gulp-file-include";
 import webpHtmlNosvg from "gulp-webp-html-nosvg";
-//import versionNumber from "gulp-version-number";
+import versionNumber from "gulp-version-number";
 
 // html
 export const html = () => {
@@ -8,5 +8,21 @@ export const html = () => {
         .pipe(fileInclude())//build html file from small parts to one file
         .pipe(global.app.plugins.replace(/@img\//g, 'img/'))// replace @img/ to img/
         .pipe(webpHtmlNosvg())//converting common <img src=...> to <picture> with additional using webp format
+        .pipe(
+            versionNumber({
+                'value': '%DT%',
+                'append': {
+                    'key': '_v',
+                    'cover': 0,
+                    'to': [
+                        'css',
+                        'js',
+                    ]
+                },
+                'output': {
+                    'file': 'gulp/version.json'
+                }
+            })
+        )       
         .pipe(global.app.gulp.dest(global.app.path.build.html));
 }
