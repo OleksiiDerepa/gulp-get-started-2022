9f5525b07448cc6925dbe7fa812f455f9f9f5e11
qiceto <qiceto.atedod@rungel.net>
2024-06-27 23:39:46 +0300

    setup SASS transformation

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -2,14 +2,10 @@ import replace from 'gulp-replace';// find and replace
 import plumber from 'gulp-plumber';// error handling
 import notify from 'gulp-notify';// messages and tips
 import browsersync from 'browser-sync';// local server
-// import newer from 'gulp-newer'
-//import rename from 'gulp-rename';
 
 export const plugins = {
   replace,
   plumber,
   notify,
   browsersync,
-  // newer,
-  //rename
 };
