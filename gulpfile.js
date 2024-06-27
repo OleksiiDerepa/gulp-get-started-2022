32265b087327add0d133ab06965a9947c9ce9fe3
qiceto <qiceto.atedod@rungel.net>
2024-06-27 13:16:59 +0300

    set up watcher

--- gulpfile.js
+++ gulpfile.js
@@ -17,5 +17,24 @@ global.app = {
 // Import tasks
 import { copy } from './gulp/tasks/copy.js';
 
+// 1) setup watcher
+// Observe changes in files
+function watcher(){
+    gulp.watch(path.watch.files, copy);
+}
+
+// 2) setup sequence of stepecs
+// PARALLEL- ASYNC MODE for our steps
+// 1 step - copy all files from source folder
+const mainTasks = gulp.parallel(copy);
+
+// 3) setup default task for dev mode
+// SERIES - SERIAL MODE for our steps
+// 1 step - copy all files from source folder
+// 2 step - watch for changes in files
+const dev = gulp.series(mainTasks, watcher);
+
+
 //Run default task by default
-gulp.task('default', copy);
+// gulp.task('default', copy);
+gulp.task('default', dev);
