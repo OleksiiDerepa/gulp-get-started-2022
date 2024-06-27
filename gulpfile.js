149df02cf4b7f4a6f7574fb0781ed39aed85c4e7
qiceto <qiceto.atedod@rungel.net>
2024-06-27 12:30:13 +0300

    import files into gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+// Main module
+import gulp from 'gulp';
+// Import our path
+import { path } from './gulp/config/path.js';
+// Import common plugins
+import { plugins } from './gulp/config/plugins.js';
+
+// Pass values to global variable
+global.app = {
+    isBuild: process.argv.includes('--build'),
+    isDev: !process.argv.includes('--build'),
+    path: path,
+    gulp: gulp,
+    plugins: plugins,
+}
