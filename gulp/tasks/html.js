47d45766476a0949ec56517c3fe446af650e128b
qiceto <qiceto.atedod@rungel.net>
2024-06-27 16:56:39 +0300

    converting img tag with using webp format automatically - install gulp-webp-html-nosvg

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,9 +1,12 @@
 import fileInclude from "gulp-file-include";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
+//import versionNumber from "gulp-version-number";
 
 // html
 export const html = () => {
     return global.app.gulp.src(global.app.path.src.html)
         .pipe(fileInclude())//build html file from small parts to one file
         .pipe(global.app.plugins.replace(/@img\//g, 'img/'))// replace @img/ to img/
+        .pipe(webpHtmlNosvg())//converting common <img src=...> to <picture> with additional using webp format
         .pipe(global.app.gulp.dest(global.app.path.build.html));
 }
