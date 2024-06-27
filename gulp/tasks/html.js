a588ff986414f2a4d96c643c66a9645714876d47
qiceto <qiceto.atedod@rungel.net>
2024-06-27 14:33:50 +0300

    build html page from all include parts - install gulp-file-include plugin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,4 +1,8 @@
+import fileInclude from "gulp-file-include";
+
+// html
 export const html = () => {
     return global.app.gulp.src(global.app.path.src.html)
+        .pipe(fileInclude())
         .pipe(global.app.gulp.dest(global.app.path.build.html));
 }
