954c849ead7313ea45d1ac0d7eb73d4def297317
qiceto <qiceto.atedod@rungel.net>
2024-06-27 17:29:34 +0300

    catch errors and show them in the browser - install gulp-plumber and gulp-notify plugin

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,7 +1,15 @@
-import replace from 'gulp-replace';
+import replace from 'gulp-replace';// find and replace
+import plumber from 'gulp-plumber';// error handling
+import notify from 'gulp-notify';// messages and tips
+// import browsersync from 'browser-sync';// local server
+// import newer from 'gulp-newer'
 //import rename from 'gulp-rename';
 
 export const plugins = {
   replace,
+  plumber,
+  notify,
+  // browsersync,
+  // newer,
   //rename
 };
--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,17 @@
 import fileInclude from "gulp-file-include";
 import webpHtmlNosvg from "gulp-webp-html-nosvg";
 import versionNumber from "gulp-version-number";
+//import pug from "gulp-pug";
 
 // html
 export const html = () => {
     return global.app.gulp.src(global.app.path.src.html)
+        .pipe(app.plugins.plumber(//catch errors and show them in the browser
+            app.plugins.notify.onError({
+                title: "HTML",
+                message: "Error: <%= error.message %>"
+            })
+        ))
         .pipe(fileInclude())//build html file from small parts to one file
         .pipe(global.app.plugins.replace(/@img\//g, 'img/'))// replace @img/ to img/
         .pipe(webpHtmlNosvg())//converting common <img src=...> to <picture> with additional using webp format
